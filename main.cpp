f8e90748c956829d19797628aff9acdd8204ed71
Kazune Takahashi <9254151938@mail.ecc.u-tokyo.ac.jp>
2015-02-22 18:05:46 +0900

    C++11は使っていいらしい

--- main.cpp
+++ main.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <tuple>
 using namespace std;
 
 class ChildStuntedness5 {
+  // 普通の
+  int testType;
+  int scenario;
+  vector<string> training;
+  vector<string> testing;
+  // 自分の
+  vector< vector<string> > test_case_st;
+  //  main theorems
 public:
-  vector<double> predict(int testType, int scenario, 
-                         vector<string> training, vector<string> testing) {
+  vector<double> predict(int _testType, int _scenario, 
+                         vector<string> _training, vector<string> _testing) {
+    testType = _testType;
+    scenario = _scenario;
+    training = _training;
+    testing = _testing;
+    make_training();
     vector<double> X;
-    cerr << "testType" << testType << endl;
-    cerr << "scenario" << scenario << endl;
-    for (unsigned int i=0; i<training.size(); i++) {
-      cerr << "no" << i << endl;
-      cerr << training[i] << endl;
-      cerr << testing[i] << endl;
-      X.push_back(90);
-    }
     return X;
+  }
+  // lemmas
+private:
+  vector<string> split(string S) { // コンマでsprit
+    vector<string> ans;
+    stringstream ss;
+    string s;
+    while(getline(ss, s, ',')) {
+      ans.push_back(s);
+    }
+    return ans;
+  }
+  void make_test_case_st() {
+    int N = testing.size();
+    for (auto i=0; i<N; i++) {
+      vector<string> sps = split(testing[i]);
+      test_case_st.push_back(sps);
+    }
+  }
+  void make_training() {
+    
   }
 };
