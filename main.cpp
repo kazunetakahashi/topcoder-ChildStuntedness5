8b73245dcce44e8e095af8c22d2754a6d1febf9d
Kazune Takahashi <kazunetakahashi@gmail.com>
2015-02-27 03:33:12 +0900

    calc_multまで

--- main.cpp
+++ main.cpp
@@ -66,7 +66,7 @@ matrix transposed(matrix A) {
   matrix B = matrix(A.col, A.row);
   for (auto i=0; i<B.row; i++) {
     for (auto j=0; j<B.col; j++) {
-      B.a[i*B.row + j] = A.a[j*B.row + i];
+      B.a[i*B.col + j] = A.a[j*B.col + i];
     }
   }
   return B;
@@ -107,8 +107,11 @@ private:
   // 自分の
   vector< vector<string> > train_case_st;
   vector< vector<double> > train_case;
+  vector< vector<double> > train_case_id;  
   vector< vector<string> > test_case_st;
   vector< vector<double> > test_case;
+  vector< vector<double> > test_case_id;
+  int num_col = 0; // mult_col[scenario].size()
   double reg_transform[100]; // reg
   double mari_transform[7];
   double A[100]; // 係数部分
@@ -116,7 +119,12 @@ private:
   double zansa[100];
   vector< pair<double, int> > ranking;
   vector<double> X; // answer
+  
   // lemmas
+  void init() {
+    num_col = mult_col[scenario].size();
+  }
+  
   vector<string> split(string S) { // コンマでsprit
     vector<string> ans;
     stringstream ss(S);
@@ -126,30 +134,16 @@ private:
     }
     return ans;
   }
-  void make_train_case_st() { // trainingをsplit
-    int N = training.size();
-    for (auto i=0; i<N; i++) {
-      vector<string> sps = split(training[i]);
-      train_case_st.push_back(sps);
-    }
-    cerr << "split completed" << endl;
+  
+  bool issameline_mult(int row, vector< vector<double> >::iterator it) {
+    return (row > 1 && it[row-1][0] == it[row][0]);
   }
-  void convert_double() { // doubleにする
-    train_case = vector< vector<double> >(train_case_st.size(),
-                                          vector<double>());
-    for (auto i=0; i<train_case_st.size(); i++) {
-      train_case[i] = vector<double>(train_case_st[i].size());
-      for (auto j=0; j<train_case_st[i].size(); j++) {
-        if (train_case_st[i][j][0] == 'N') {
-          train_case[i][j] = NA;
-        } else {
-          // cerr << train_case_st[i][j] << endl;
-          train_case[i][j] = stof(train_case_st[i][j]);
-        }
-      }
-    }
-    cerr << "convert completed" << endl;
+  
+  bool issameline(int row, int col, vector< vector<double> >::iterator it) {
+    return (row > 1 && it[row-1][0] == it[row][0]
+            && it[row-1][col] == it[row][col]);
   }
+  
   void fill_iq() { // iq埋める
     int iq = NA;
     unsigned int ind = 0;
@@ -168,10 +162,72 @@ private:
     }
     cerr << "iq filled" << endl;
   }
-  bool issameline(int row, int col, vector< vector<double> >::iterator it) {
-    return (row > 1 && it[row-1][0] == it[row][0]
-            && it[row-1][col] == it[row][col]);
+  
+  void make_train_case() {
+    int N = training.size();
+    for (auto i=0; i<N; i++) {
+      vector<string> sps = split(training[i]);
+      train_case_st.push_back(sps);
+    }
+    cerr << "split completed" << endl;
+    train_case = vector< vector<double> >(train_case_st.size(),
+                                          vector<double>());
+    for (auto i=0; i<train_case_st.size(); i++) {
+      train_case[i] = vector<double>(train_case_st[i].size());
+      for (auto j=0; j<train_case_st[i].size(); j++) {
+        if (train_case_st[i][j][0] == 'N') {
+          train_case[i][j] = NA;
+        } else {
+          train_case[i][j] = stof(train_case_st[i][j]);
+        }
+      }
+    }
+    cerr << "convert completed" << endl;
+    fill_iq(); // ここだけtrainに必要！！
   }
+  
+  void make_train_case_id() {
+    int N = training.size();
+    for (auto i=0; i<N; i++) {
+      if (!(issameline_mult(i, train_case.begin()))) {
+        train_case_id.push_back(train_case[i]);
+      }
+    }
+    cerr << "id sorted" << endl;
+  }
+  
+  void make_test_case() {
+    int N = testing.size();
+    for (auto i=0; i<N; i++) {
+      vector<string> sps = split(testing[i]);
+      test_case_st.push_back(sps);
+    }
+    cerr << "split completed" << endl;
+    test_case = vector< vector<double> >(test_case_st.size(),
+                                          vector<double>());
+    for (auto i=0; i<test_case_st.size(); i++) {
+      test_case[i] = vector<double>(test_case_st[i].size());
+      for (auto j=0; j<test_case_st[i].size(); j++) {
+        if (test_case_st[i][j][0] == 'N') {
+          test_case[i][j] = NA;
+        } else {
+          test_case[i][j] = stof(test_case_st[i][j]);
+        }
+      }
+    }
+    cerr << "convert completed" << endl;
+  }
+  
+  void make_test_case_id() {
+    int N = testing.size();
+    for (auto i=0; i<N; i++) {
+      if (!(issameline_mult(i, test_case.begin()))) {
+        test_case_id.push_back(test_case[i]);
+      }
+    }
+    cerr << "id sorted" << endl;
+  }
+  
   void average_col(int col, double* tra, int siz) {
     double sum[100];
     int cou[100];
@@ -204,6 +260,7 @@ private:
       tra[temp[i].second] = i;
     }
   }
+  
   void transform(vector< vector<double> >::iterator b,
                  vector< vector<double> >::iterator e) {
     // 結婚状態の数値を適正化。
@@ -306,6 +363,7 @@ private:
       // if ((*t)[region_col] != NA) cerr << (*t)[region_col] << endl;
     }
   }
+  
   void calc_AB(int col) { // ABする
     int n = 0;
     double sumx = 0;
@@ -332,6 +390,7 @@ private:
     A[col] = (n * sumxy - sumx * sumy) / (n * sumx2 - sumx * sumx);
     B[col] = (sumx2 * sumy - sumxy * sumx) / (n * sumx2 - sumx * sumx);
   }
+  
   void zansa_2jouwa(int col) {
     zansa[col] = 0;
     int n = 0;
@@ -350,6 +409,37 @@ private:
     zansa[col] /= n;
     cerr << col << ": " << (long long)zansa[col] << endl;
   }
+  
+  matrix calc_mult(int id) {
+    int n = 0;
+    bool useid = (scenario != 1);
+    for (auto i=0; i<num_col; i++) {
+      if( (id >> i & 1) == 1) n++;
+    }
+    int r = (useid ? test_case_id.size() : test_case.size());
+    matrix X(r, n+1);
+    vector< vector<double> >::iterator it =
+      (useid ? test_case_id.begin() : test_case.begin());
+    int m = 0;
+    for (auto i=0; i<num_col; i++) {
+      if( (id >> i & 1) == 1) {
+        for (auto j=0; j<r; j++) {
+          X.a[j * X.col + m] = it[j][mult_col[scenario][i]];
+        }
+        m++;
+      }
+    }
+    for (auto j=0; j<r; j++) {
+      X.a[j * X.col + m] = 1;
+    }
+    matrix y(r, 1);
+    for (auto j=0; j<r; j++) {
+      y.a[j] = it[j][IQ_col];
+    }
+    matrix Xt = transposed(X);
+    return inverse(multiply(Xt, X), multiply(Xt, y));
+  }
+  
   void calc_all() {
     for (auto i=0; i<=scenario; i++) {
       for (auto j=0; j<used_col[i].size(); j++) {
@@ -361,30 +451,7 @@ private:
     }
     sort(ranking.begin(), ranking.end());
   }
-  void make_test_case_st() { // testingをsplit
-    int N = testing.size();
-    for (auto i=0; i<N; i++) {
-      vector<string> sps = split(testing[i]);
-      test_case_st.push_back(sps);
-    }
-    cerr << "split completed" << endl;
-  }
-  void convert_double_test() { // doubleにする
-    test_case = vector< vector<double> >(test_case_st.size(),
-                                          vector<double>());
-    for (auto i=0; i<test_case_st.size(); i++) {
-      test_case[i] = vector<double>(test_case_st[i].size());
-      for (auto j=0; j<test_case_st[i].size(); j++) {
-        if (test_case_st[i][j][0] == 'N') {
-          test_case[i][j] = NA;
-        } else {
-          // cerr << test_case_st[i][j] << endl;
-          test_case[i][j] = stof(test_case_st[i][j]);
-        }
-      }
-    }
-    cerr << "convert completed" << endl;
-  }
+  
   void make_X() {
     vector< pair<int, int> > temp; // id, iq 
     for (auto i=0; i<test_case.size(); i++) {
@@ -416,21 +483,23 @@ private:
       X.push_back(temp[i].second);
     }
   }
+  
   void make_training() {
-    make_train_case_st();
-    convert_double();
-    fill_iq();
+    make_train_case();
     average_col(mari_col, mari_transform, 7);
     average_col(region_col, reg_transform, 100);
     transform(train_case.begin(), train_case.end());
+    make_train_case_id();
     calc_all();
   }
+  
   void make_testing() {
-    make_test_case_st();
-    convert_double_test();
+    make_test_case();
     transform(test_case.begin(), test_case.end());
+    make_test_case_id();
     make_X();
   }
+  
   //  main theorems
 public:
   vector<double> predict(int _testType, int _scenario, 
@@ -439,6 +508,7 @@ public:
     scenario = _scenario;
     training = _training;
     testing = _testing;
+    init();
     make_training();
     make_testing();
     return X;
