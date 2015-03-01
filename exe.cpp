7d6254c2d70a6229ecb168778580405483b1ba52
Kazune Takahashi <kazunetakahashi@gmail.com>
2015-03-01 23:42:00 +0900

    fixed again

--- exe.cpp
+++ exe.cpp
@@ -9,9 +9,11 @@ int main() {
   while (getline(ifs, str)) {
     T.push_back(str);
   }
-  ChildStuntedness5 c;
-  vector<double> ans = c.predict(0, 2, T, T);
-  for (auto i=0; i<ans.size(); i++) {
-    // cout << ans[i] << endl;
+  for (auto k=0; k<3; k++) {
+    ChildStuntedness5 c;
+    vector<double> ans = c.predict(0, k, T, T);
+    for (auto i=0; i<ans.size(); i++) {
+      // cout << ans[i] << endl;
+    }
   }
 }
